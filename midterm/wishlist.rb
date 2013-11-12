c8436c76d3c8d0dd64ac336f2f37595c579be3b6
Doug MacDowell <dougmac9@uw.edu>
2013-11-12 11:36:44 -0800

    initial checkin of wishlist class file

---
+++ midterm/wishlist.rb
@@ -0,0 +1,8 @@
+class WishList
+
+#   - The WishList class should:
+#      - Mixin Enumerable
+#      - Define each so it returns wishes as strings with their index as part of the string
+
+
+# add stuff
